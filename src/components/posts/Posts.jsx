d63b2e9eaf871b7d0ff2d1e7f45f0dc84727acdf
BPark44 <baileyparkerb@gmail.com>
2023-01-13 10:42:51 +1100

    Backend progress

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -3,25 +3,6 @@ import Post from "../post/Post";
 
 const Posts = () => {
   //TEMP
-  const posts = [
-    {
-      id: 1,
-      name: "Elon Musk",
-      userId: 1,
-      profilePic:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
-      desc: "Launch!",
-      img: "http://spaceflightnow.com/wp-content/uploads/2020/01/49399916862_cd676f67f6_o-copy.jpg",
-    },
-    {
-      id: 2,
-      name: "Elon Musk",
-      userId: 1,
-      profilePic:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg",
-      desc: "Hello and welcome to my first post...",
-    },
-  ];
 
   return (
     <div className="posts">
